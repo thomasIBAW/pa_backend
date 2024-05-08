a5cfdc4517969783184bdbd5ce874406985ac46e
Thomas <tbe@pm.me>
2024-05-09 00:36:16 +0200

    testing logout

--- routes/logout.js
+++ routes/logout.js
@@ -1,27 +1,30 @@
 import express from "express";
-import {write, findAll, findOne, deleteOne, patchOne, findSome} from "../connectors/dbConnector.js";
-import {logger} from '../middlewares/loggers.js'
-import jwt from 'jsonwebtoken';
-import date from 'date-and-time';
-import { todoSchema } from "../classes/schemas.js";
+// import {write, findAll, findOne, deleteOne, patchOne, findSome} from "../connectors/dbConnector.js";
+// import {logger} from '../middlewares/loggers.js'
+// import jwt from 'jsonwebtoken';
+// import date from 'date-and-time';
+// import { todoSchema } from "../classes/schemas.js";
 import 'dotenv/config'
-import bcrypt from "bcrypt";
+// import bcrypt from "bcrypt";
 
 // secret needed to compare login password with the hash in the DB
 // Please change the secret to any random string in the .env file
 const secret = process.env.mySecret  //to be set in Env variables
 
 const router = express.Router();
-const collection = "todos";
 
 router.post('/', (req, res) => {
 
     {
+        console.log("received logout request ... ")
+
         res.clearCookie('token', {
             sameSite: 'strict',
-            httpOnly: false
+            httpOnly: true
         })
-        res.send({ success: true, message: 'Cookie deleted'});
+        console.log("token removed- Send code 200 ... ")
+
+        res.status(200).json({message: 'Cookie deleted'});
     }
 
 })
