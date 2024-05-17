c267d3ff73d16d01a7c58d326dbbffad965df069
Thomas <tbe@pm.me>
2024-05-18 00:46:35 +0200

    created endpoint for InvitationCode creation

--- routes/family.js
+++ routes/family.js
@@ -151,7 +151,6 @@ router.patch('/:uuid', addColl, identUser, getFamilyCheck, checkUserInFamily, (r
     if (!req.body) return res.status(404).json({ message: 'Missing body...'})
     let details = {fileName:"family.js", isAdmin: req.isAdmin, isUserFamilyAdmin: req.isUserFamilyAdmin, isUserFamilyMember:req.isUserFamilyMember}
 
-    // only Server Admins can delete Family
     if (!req.isAdmin) {
 
         if(req.isUserFamilyAdmin) {
@@ -193,6 +192,74 @@ router.patch('/:uuid', addColl, identUser, getFamilyCheck, checkUserInFamily, (r
 
 })
 
+
+// Endpoint to add an invitation code
+router.post("/code", addColl, identUser, async (req, res) =>{
+    let details = {fileName:"family.js", isAdmin: req.isAdmin, isUserFamilyAdmin: req.isUserFamilyAdmin, isUserFamilyMember:req.isUserFamilyMember}
+
+
+    logger.debug("reached the new Family router for invitationcode creation...", details)
+    let session_familyUuid = ""
+
+    try {
+        let val = {};
+        
+        function isValidCode(code) {
+            const regex = /^[A-Za-z0-9]{6}$/;
+            return regex.test(code);
+          } 
+
+        if (!isValidCode(req.body.invitationCode)) {
+            logger.error(`The provided InvitationCode is not valid or missing. Aborting ...`, details)
+            res.status(404).json({message: `The provided InvitationCode is not valid or missing. Aborting ...`})
+        }
+        let filter = {}
+        filter.uuid = req.body.uuid
+
+        const familyToUpdate = await findSome("family", filter)
+            .then((d) => {
+                logger.debug(`${collection} - Received a list request from User ${req.decoded.username} `, details);
+                return d
+            })
+            .catch((err) => {
+                logger.error(err)
+                res.status(404).json(err)
+            })
+         console.log(familyToUpdate[0])   
+
+        logger.debug(`adding Invitation Code ${req.body.invitationCode} to Family ${familyToUpdate[0].familyName}`)
+
+        if (!familyToUpdate[0].invitationCode || familyToUpdate[0].invitationCode.length === 0 ) { 
+            val.invitationCode = [{code: req.body.invitationCode, createdBy: req.decoded.userUuid, dateCreated: new Date(), valid: true, usedBy: null}]
+        } else {
+            val.invitationCode = [...familyToUpdate[0].invitationCode,{code: req.body.invitationCode, createdBy: req.decoded.userUuid, dateCreated: new Date(), valid: true, usedBy: null}]
+        }
+
+        logger.debug(`new List of invitation codes : ${val.invitationCode}`, details)
+
+        //TODO Add Permission Logic, that only Admin or Family Admin can issue Codes. 
+
+        patchOne("family", familyToUpdate[0].uuid, val)
+        .then((d) => {
+            logger.warn(`Updated in collection "family" entry: ${familyToUpdate[0].uuid} by: ... `, details)
+            
+            // Adding a socket message to update all open pages
+            // Socket updates a useless state on all connected clients on the pages identified by the collection.
+            // The updated state triggers a page reload so that any new item immediately appears on the client.
+            io.to(familyToUpdate[0].uuid).emit("family", new Date());
+            res.status(200).json(d)})
+
+        .catch((err) => {
+            logger.error(err)
+            res.status(404).json(err)})
+
+    }
+    catch (err) {
+        logger.error(`Error in middlewares.js, on the post item function - ${err.message}`, details)
+        res.status(404).json(err.message)
+    }
+
+});
 
 export default router
 
